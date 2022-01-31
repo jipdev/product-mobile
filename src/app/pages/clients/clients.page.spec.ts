5f8c238b6e18e3fa873edc1f2f9110d9ebb0b9ea
Jhonattan Loli Mazzuco <jhonattanloli@gmail.com>
2022-01-31 10:05:35 +0044

    Preparado módulo de cliente

--- src/app/pages/clients/clients.page.spec.ts
+++ src/app/pages/clients/clients.page.spec.ts
@@ -1,23 +1,77 @@
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { NavigationEnd, Router, RouterEvent } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
+import { Subject } from 'rxjs';
 import { ClientsPage } from './clients.page';
+import { ROOT_CLIENTS_URL } from './constants/root-client-url';
+
+const routerEventSubject = new Subject<RouterEvent>();
 
 describe('ClientsPage', () => {
   let component: ClientsPage;
   let fixture: ComponentFixture<ClientsPage>;
+  let router: Router;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ClientsPage],
-      imports: [IonicModule.forRoot()]
+      imports: [IonicModule.forRoot()],
+      providers: [
+        {
+          provide: Router,
+          useValue: {
+            navigateByUrl: (url: string) => null,
+            events: routerEventSubject
+          }
+        }
+      ],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA]
     }).compileComponents();
 
     fixture = TestBed.createComponent(ClientsPage);
     component = fixture.componentInstance;
+    router = TestBed.inject(Router);
     fixture.detectChanges();
   }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should be ngOnInit', () => {
+    const listenRouteEventSpy = spyOn(component, 'listenRouteEvent').and.stub();
+
+    component.ngOnInit();
+
+    expect(listenRouteEventSpy).toHaveBeenCalled();
+  });
+
+  it('should be listenRouteEvent', () => {
+    const mock = new NavigationEnd(1, '/test', '/test');
+
+    const onNavigateSpy = spyOn(component, 'onNavigate').and.stub();
+
+    component.listenRouteEvent();
+
+    routerEventSubject.next(mock);
+
+    expect(onNavigateSpy).toHaveBeenCalledWith(mock);
+  });
+
+  it('should be onNavigate', () => {
+    const expectedUrl = '/test';
+
+    component.onNavigate(new NavigationEnd(1, expectedUrl, expectedUrl));
+
+    expect(component.activeUrl).toEqual(expectedUrl);
+  });
+
+  it('should be back', () => {
+    const navigateByUrlSpy = spyOn(router, 'navigateByUrl').and.stub();
+
+    component.back();
+
+    expect(navigateByUrlSpy).toHaveBeenCalledWith(ROOT_CLIENTS_URL);
   });
 });
--- src/app/pages/clients/clients.page.ts
+++ src/app/pages/clients/clients.page.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
+import { ROOT_CLIENTS_URL } from './constants/root-client-url';
 
 @Component({
   selector: 'app-clients',
   templateUrl: 'clients.page..html',
   styleUrls: ['clients.page.scss']
 })
-export class ClientsPage {
+export class ClientsPage implements OnInit {
+  activeUrl: string;
 
-  constructor() {
+  constructor(private router: Router, private cd: ChangeDetectorRef) {
   }
 
+  get isRootUrl(): boolean {
+    return this.activeUrl === ROOT_CLIENTS_URL;
+  }
+
+  ngOnInit(): void {
+    this.listenRouteEvent();
+  }
+
+  listenRouteEvent(): void {
+    this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe({
+      next: event => this.onNavigate(event as NavigationEnd)
+    });
+  }
+
+  onNavigate(event: NavigationEnd): void {
+    this.activeUrl = event.urlAfterRedirects;
+    this.cd.detectChanges();
+  }
+
+  back(): void {
+    this.router.navigateByUrl(ROOT_CLIENTS_URL);
+  }
 }
